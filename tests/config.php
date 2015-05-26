e6f01e7ae54e8bf0fd06b0ede8fdee7d4baaa036
Nils Le Roux <nleroux@payplug.com>
2015-05-26 10:52:53 +0200

    Fixed default timezone warning in the logs during tests.

--- tests/config.php
+++ tests/config.php
@@ -2,8 +2,10 @@
 
 require_once 'lib/PayPlug.php';
 
+date_default_timezone_set('UTC');
+
 class TestsConfig
 {
-    const LIVE_TOKEN = 'sk_live_512fd6873f246339517d00180f7e5dfe';
-    const TEST_TOKEN = 'sk_test_b3baa16442266f941427dac365a4dd72';
+    const LIVE_TOKEN = '';
+    const TEST_TOKEN = '';
 }
