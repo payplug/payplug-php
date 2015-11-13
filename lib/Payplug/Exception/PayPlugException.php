f05bbbe2d2a9af492f1ca08ee03dc6fd83a626f9
Nils LR <nleroux@payplug.com>
2015-11-13 12:13:26 +0100

    Fixed bug with case insensitive filesystems.

--- lib/Payplug/Exception/PayPlugException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Payplug\Exception;
-
-trigger_error(
-    'PayPlugException is deprecated and may be removed in a near future. Use PayplugException instead.',
-    E_USER_DEPRECATED
-);
-
-require_once(__DIR__ . DIRECTORY_SEPARATOR . 'PayplugException.php');
--- lib/Payplug/Exception/PayPlugServerException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Payplug\Exception;
-
-trigger_error(
-    'PayPlugServerException is deprecated and may be removed in a near future. Use PayplugServerException instead.',
-    E_USER_DEPRECATED
-);
-
-require_once(__DIR__ . DIRECTORY_SEPARATOR . 'PayplugServerException.php');
