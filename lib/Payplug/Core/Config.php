e148d9794539f0260c3859fc98b9cac9a3591128
NLR <nleroux@payplug.com>
2016-03-08 18:14:15 +0100

    Do not check OpenSSL version since TLS v1.0 is supported. Remove incorrect require-dev option in tests README.

--- lib/Payplug/Core/Config.php
+++ lib/Payplug/Core/Config.php
@@ -10,7 +10,7 @@ class Config
     /**
      * The library version
      */
-    const LIBRARY_VERSION = '2.2.0';
+    const LIBRARY_VERSION = '2.2.1';
 
     /**
      * PHP minimal version required by this library
@@ -37,14 +37,6 @@ class Config
 // Check PHP version
 if (version_compare(phpversion(), Config::PHP_MIN_VERSION, "<")) {
     throw new \RuntimeException('This library requires PHP ' . Config::PHP_MIN_VERSION . ' or newer.');
-}
-
-// Check OpenSSL version
-// http://blog.techstacks.com/2013/04/an-openssl-version-matrix.html
-if (defined('OPENSSL_VERSION_NUMBER') && OPENSSL_VERSION_NUMBER < 0x10001001) {
-    throw new Exception\DependencyException(
-        'This library requires OpenSSL 1.0.1 or newer. Please contact your webhoster or update your OpenSSL version.'
-    );
 }
 
 // Check PHP configuration
