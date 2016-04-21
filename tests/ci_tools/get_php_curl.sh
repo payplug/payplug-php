1f62123cf47efff8e7fd3922e7004334fc15e0b0
Nils LR <nleroux@payplug.com>
2016-04-21 15:22:41 +0200

    Add Travis CI

--- tests/ci_tools/get_php_curl.sh
+++ tests/ci_tools/get_php_curl.sh
@@ -1,5 +1,6 @@
-#!/bin/bash -v
+#!/bin/bash
 
+set -x
 set -e
 
 if [ $# -lt 2 ]; then
@@ -15,11 +16,15 @@ PHP_CURL_LIBS_DIRECTORY="$PHP_DIRECTORY/$CURL_VERSION"
 if [ -f "$PHP_CURL_LIBS_DIRECTORY/phpcurl/curl.so" ] \
    && [ -f "$PHP_CURL_LIBS_DIRECTORY/curl/lib/libcurl.so" ] \
    && [ -d "$PHP_DIRECTORY/php" ]; then
-    echo "Curl shared objects already exists."
+    echo "Curl shared objects were cached. Skipping."
     exit 0
 fi
 
-mkdir -p "$PHP_CURL_LIBS_DIRECTORY/phpcurl" "$PHP_CURL_LIBS_DIRECTORY/php" "$PHP_CURL_LIBS_DIRECTORY/curl"
+SCRIPT_PATH="$(readlink --canonicalize-existing "$0")"
+DIR_PATH="$(dirname "$SCRIPT_PATH")"
+source "$DIR_PATH/travis_wait.sh"
+
+mkdir -p "$PHP_CURL_LIBS_DIRECTORY/phpcurl" "$PHP_DIRECTORY/php" "$PHP_CURL_LIBS_DIRECTORY/curl"
 
 TEMP_DIRECTORY="$(mktemp -d)"
 
@@ -38,14 +43,18 @@ make
 make install
 
 # Compile PHP
-grep -v "with-curl" ~/.phpenv/plugins/php-build/share/php-build/default_configure_options
-     > ~/.phpenv/plugins/php-build/share/php-build/default_configure_options
-~/.phpenv/plugins/php-build/bin/php-build "$PHP_VERSION" "$PHP_DIRECTORY/php"
+cd ..
+git clone https://github.com/php-build/php-build.git
+grep -v "with-curl" "php-build/share/php-build/default_configure_options" \
+     > "php-build/share/php-build/default_configure_options_"
+mv "php-build/share/php-build/default_configure_options_" "php-build/share/php-build/default_configure_options"
+travis_wait 30 php-build/bin/php-build "$PHP_VERSION" "$PHP_DIRECTORY/php"
 
 # Compile PHP Curl
-cd "../php-src-php-$PHP_VERSION/ext/curl/"
-phpize
-./configure --with-curl="$PHP_CURL_LIBS_DIRECTORY/curl/"
+cd "php-src-php-$PHP_VERSION/ext/curl/"
+chmod +x "$PHP_DIRECTORY/php/bin/phpize"
+"$PHP_DIRECTORY/php/bin/phpize"
+./configure --with-curl="$PHP_CURL_LIBS_DIRECTORY/curl/" --with-php-config="$PHP_DIRECTORY/php/bin/php-config"
 make
 cp modules/* "$PHP_CURL_LIBS_DIRECTORY/phpcurl/"
 
