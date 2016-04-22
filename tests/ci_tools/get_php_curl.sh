21341574711a299844c50fa7c7c78f77ec02ce02
Nils LR <nleroux@payplug.com>
2016-04-22 09:54:31 +0200

    Increase Travis Cache + refactor directory organisation of php-curl stuff.

--- tests/ci_tools/get_php_curl.sh
+++ tests/ci_tools/get_php_curl.sh
@@ -10,11 +10,11 @@
 # - Check that PHP binaries, libcurl and PHP curl extension (for this version of PHP and curl) are not cached already.
 #   If so, we have nothing to do.
 # - Download curl (from curl official website) and PHP sources (from php/php-src repo).
-# - Build curl into /tmp/PHP_VERSION/CURL_VERSION/curl/
-# - Build php-curl shared object (curl.so) into /tmp/PHP_VERSION/CURL_VERSION/phpcurl/
+# - Build curl into /tmp/curl/curl/CURL_VERSION
+# - Build php-curl shared object (curl.so) into /tmp/curl/php-curl/PHP_VERSION/CURL_VERSION
 # - Download php-build and patch it to drop the --with-curl option.
-# - Build PHP into /tmp/PHP_VERSION/php/
-# - Update php.ini in /tmp/PHP_VERISON/php/etc/php.ini to load curl module (that we previously build).
+# - Build PHP into /tmp/curl/php/PHP_VERSION/
+# - Update php.ini in /tmp/curl/php/PHP_VERISON/etc/php.ini to load curl module (that we previously build).
 
 set -x
 set -e
@@ -26,12 +26,14 @@ fi
 
 CURL_VERSION="$1"
 PHP_VERSION="$2"
-PHP_DIRECTORY="/tmp/curl/$PHP_VERSION"
-PHP_CURL_LIBS_DIRECTORY="$PHP_DIRECTORY/$CURL_VERSION"
+ROOT_DIRECTORY="/tmp/curl"
+PHP_DIRECTORY="$ROOT_DIRECTORY/php/$PHP_VERSION"
+CURL_DIRECTORY="$ROOT_DIRECTORY/curl/$CURL_VERSION"
+PHP_CURL_DIRECTORY="$ROOT_DIRECTORY/php-curl/$PHP_VERSION/$CURL_VERSION"
 
-if [ -f "$PHP_CURL_LIBS_DIRECTORY/phpcurl/curl.so" ] \
-   && [ -f "$PHP_CURL_LIBS_DIRECTORY/curl/lib/libcurl.so" ] \
-   && [ -d "$PHP_DIRECTORY/php" ]; then
+if [ -f "$PHP_CURL_DIRECTORY/curl.so" ] \
+   && [ -f "$CURL_DIRECTORY/lib/libcurl.so" ] \
+   && [ -d "$PHP_DIRECTORY/bin" ]; then
     echo "Curl shared objects were cached. Skipping."
     exit 0
 fi
@@ -40,19 +42,19 @@ SCRIPT_PATH="$(readlink --canonicalize-existing "$0")"
 DIR_PATH="$(dirname "$SCRIPT_PATH")"
 source "$DIR_PATH/travis_wait.sh"
 
-mkdir -p "$PHP_CURL_LIBS_DIRECTORY/phpcurl" "$PHP_DIRECTORY/php" "$PHP_CURL_LIBS_DIRECTORY/curl"
+mkdir -p "$PHP_DIRECTORY" "$CURL_DIRECTORY" "$PHP_CURL_DIRECTORY"
 
 TEMP_DIRECTORY="$(mktemp -d)"
 
 cd "$TEMP_DIRECTORY"
 
 # Build curl
-if ! [ -f "$PHP_CURL_LIBS_DIRECTORY/curl/lib/libcurl.so" ]; then
+if ! [ -f "$CURL_DIRECTORY/lib/libcurl.so" ]; then
     curl -L "https://curl.haxx.se/download/curl-$CURL_VERSION.tar.gz" --output curl.tar.gz
     tar -xzf curl.tar.gz
     cd "curl-$CURL_VERSION"
 
-    ./configure --prefix="$PHP_CURL_LIBS_DIRECTORY/curl/"
+    ./configure --prefix="$CURL_DIRECTORY"
     make
     make install
 
@@ -61,31 +63,31 @@ fi
 # End build curl
 
 # Build PHP
-if ! [ -d "$PHP_DIRECTORY/php/bin/" ]; then
+if ! [ -d "$PHP_DIRECTORY/bin" ]; then
     git clone https://github.com/php-build/php-build.git
     grep -v "with-curl" "php-build/share/php-build/default_configure_options" \
          > "php-build/share/php-build/default_configure_options_"
     mv "php-build/share/php-build/default_configure_options_" "php-build/share/php-build/default_configure_options"
-    travis_wait 30 php-build/bin/php-build "$PHP_VERSION" "$PHP_DIRECTORY/php"
+    travis_wait 30 php-build/bin/php-build "$PHP_VERSION" "$PHP_DIRECTORY"
 fi
 # End build PHP
 
 # Build PHP Curl
-if ! [ -f "$PHP_CURL_LIBS_DIRECTORY/phpcurl/curl.so" ]; then
+if ! [ -f "$PHP_CURL_DIRECTORY/curl.so" ]; then
     curl -L "https://github.com/php/php-src/archive/php-$PHP_VERSION.tar.gz" --output php.tar.gz
     tar -xzf php.tar.gz
 
     cd "php-src-php-$PHP_VERSION/ext/curl/"
-    chmod +x "$PHP_DIRECTORY/php/bin/phpize"
-    "$PHP_DIRECTORY/php/bin/phpize"
-    ./configure --with-curl="$PHP_CURL_LIBS_DIRECTORY/curl/" --with-php-config="$PHP_DIRECTORY/php/bin/php-config"
+    chmod +x "$PHP_DIRECTORY/bin/phpize"
+    "$PHP_DIRECTORY/bin/phpize"
+    ./configure --with-curl="$CURL_DIRECTORY" --with-php-config="$PHP_DIRECTORY/bin/php-config"
     make
-    cp modules/* "$PHP_CURL_LIBS_DIRECTORY/phpcurl/"
+    cp modules/* "$PHP_CURL_DIRECTORY"
 
     # Add PHP Curl module to php.ini if it's not already present
-    EXTENSION_LINE="extension=/tmp/curl/$PHP_VERSION/$CURL_VERSION/phpcurl/curl.so"
-    PHP_INI_FILE="$PHP_DIRECTORY/php/etc/php.ini"
-    sed -i '/extension=.*curl.so.*/d' "$PHP_INI_FILE"
+    EXTENSION_LINE="extension=$PHP_CURL_DIRECTORY/curl.so"
+    PHP_INI_FILE="$PHP_DIRECTORY/etc/php.ini"
+    sed -i '/extension=.*curl.so/d' "$PHP_INI_FILE"
     echo "$EXTENSION_LINE" >> "$PHP_INI_FILE"
 
     cd ..
