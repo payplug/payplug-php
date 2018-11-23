0d51422625a62d91333634138dc762f7d3906c08
Rémy Turpin <rturpin@payplug.com>
2018-11-23 16:17:38 +0100

    Add new exceptions for installment plan

---
+++ lib/Payplug/Exception/InvalidInstallmentPlanException.php
@@ -0,0 +1,9 @@
+<?php
+namespace Payplug\Exception;
+
+/**
+ * Invalid InvalidInstallmentPlanException object.
+ */
+class InvalidInstallmentPlanException extends PayplugException
+{
+}
---
+++ lib/Payplug/Exception/NotImplementedException.php
@@ -0,0 +1,9 @@
+<?php
+namespace Payplug\Exception;
+
+/**
+ * 501 Not Implemented
+ */
+class NotImplementedException extends HttpException
+{
+}
