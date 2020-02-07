3526b1bb50dc4954897599ba63d2975f11502f02
hadrien de laforce <hdelaforce@payplug.com>
2020-02-07 16:24:02 +0100

    Add OneySimulationTest & update AuthenticationTest

--- tests/unit_tests/Resource/OneySimulationResourceTest.php
+++ tests/unit_tests/Resource/OneySimulationResourceTest.php
@@ -11,7 +11,7 @@ class OneySimulationResourceTest extends \PHPUnit_Framework_TestCase
 {
     public function testGetOneySimulation3xWithFees()
     {
-        $simulation = OneySimulationResource::fromAttributes(array(
+        $simulations = OneySimulationResource::fromAttributes(array(
             'x3_with_fees' => array(
                 'effective_annual_percentage_rate' => 19.25,
                 'nominal_annual_percentage_rate' => 17.74,
@@ -30,13 +30,13 @@ class OneySimulationResourceTest extends \PHPUnit_Framework_TestCase
             )
         ));
 
-        $this->assertObjectHasAttribute('x3_with_fees', $simulation);
-        $this->assertEquals(true, is_array($simulation->x3_with_fees));
-        $this->assertEquals(19.25, $simulation->x3_with_fees['effective_annual_percentage_rate']);
-        $this->assertEquals(17.74, $simulation->x3_with_fees['nominal_annual_percentage_rate']);
-        $this->assertEquals(732, $simulation->x3_with_fees['total_cost']);
+        $this->assertObjectHasAttribute('x3_with_fees', $simulations);
+        $this->assertEquals(true, is_array($simulations->x3_with_fees));
+        $this->assertEquals(19.25, $simulations->x3_with_fees['effective_annual_percentage_rate']);
+        $this->assertEquals(17.74, $simulations->x3_with_fees['nominal_annual_percentage_rate']);
+        $this->assertEquals(732, $simulations->x3_with_fees['total_cost']);
 
-        $installments = $simulation->x3_with_fees['installments'];
+        $installments = $simulations->x3_with_fees['installments'];
 
         $this->assertEquals(2, count($installments));
 
@@ -45,12 +45,12 @@ class OneySimulationResourceTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('2020-04-06T00:00:00.000Z', $installments[1]['date']);
         $this->assertEquals(16833, $installments[1]['amount']);
 
-        $this->assertEquals(17565, $simulation->x3_with_fees['down_payment_amount']);
+        $this->assertEquals(17565, $simulations->x3_with_fees['down_payment_amount']);
     }
 
     public function testGetOneySimulation4xWithFees()
     {
-        $simulation = OneySimulationResource::fromAttributes(array(
+        $simulations = OneySimulationResource::fromAttributes(array(
             'x4_with_fees' => array(
                 'effective_annual_percentage_rate' => 19.25,
                 'nominal_annual_percentage_rate' => 17.74,
@@ -73,13 +73,13 @@ class OneySimulationResourceTest extends \PHPUnit_Framework_TestCase
             )
         ));
 
-        $this->assertObjectHasAttribute('x4_with_fees', $simulation);
-        $this->assertEquals(true, is_array($simulation->x4_with_fees));
-        $this->assertEquals(19.25, $simulation->x4_with_fees['effective_annual_percentage_rate']);
-        $this->assertEquals(17.74, $simulation->x4_with_fees['nominal_annual_percentage_rate']);
-        $this->assertEquals(732, $simulation->x4_with_fees['total_cost']);
+        $this->assertObjectHasAttribute('x4_with_fees', $simulations);
+        $this->assertEquals(true, is_array($simulations->x4_with_fees));
+        $this->assertEquals(19.25, $simulations->x4_with_fees['effective_annual_percentage_rate']);
+        $this->assertEquals(17.74, $simulations->x4_with_fees['nominal_annual_percentage_rate']);
+        $this->assertEquals(732, $simulations->x4_with_fees['total_cost']);
 
-        $installments = $simulation->x4_with_fees['installments'];
+        $installments = $simulations->x4_with_fees['installments'];
 
         $this->assertEquals(3, count($installments));
 
@@ -90,12 +90,12 @@ class OneySimulationResourceTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('2020-05-06T00:00:00.000Z', $installments[2]['date']);
         $this->assertEquals(16833, $installments[2]['amount']);
 
-        $this->assertEquals(17565, $simulation->x4_with_fees['down_payment_amount']);
+        $this->assertEquals(17565, $simulations->x4_with_fees['down_payment_amount']);
     }
 
     public function testGetOneySimulationWithFees()
     {
-        $simulation = OneySimulationResource::fromAttributes(array(
+        $simulations = OneySimulationResource::fromAttributes(array(
             'x3_with_fees' => array(
                 'effective_annual_percentage_rate' => 19.25,
                 'nominal_annual_percentage_rate' => 17.74,
@@ -135,13 +135,13 @@ class OneySimulationResourceTest extends \PHPUnit_Framework_TestCase
         ));
 
         // check 3x with fees
-        $this->assertObjectHasAttribute('x3_with_fees', $simulation);
-        $this->assertEquals(true, is_array($simulation->x3_with_fees));
-        $this->assertEquals(19.25, $simulation->x3_with_fees['effective_annual_percentage_rate']);
-        $this->assertEquals(17.74, $simulation->x3_with_fees['nominal_annual_percentage_rate']);
-        $this->assertEquals(732, $simulation->x3_with_fees['total_cost']);
+        $this->assertObjectHasAttribute('x3_with_fees', $simulations);
+        $this->assertEquals(true, is_array($simulations->x3_with_fees));
+        $this->assertEquals(19.25, $simulations->x3_with_fees['effective_annual_percentage_rate']);
+        $this->assertEquals(17.74, $simulations->x3_with_fees['nominal_annual_percentage_rate']);
+        $this->assertEquals(732, $simulations->x3_with_fees['total_cost']);
 
-        $installments_x3 = $simulation->x3_with_fees['installments'];
+        $installments_x3 = $simulations->x3_with_fees['installments'];
 
         $this->assertEquals(2, count($installments_x3));
         $this->assertEquals('2020-03-06T01:00:00.000Z', $installments_x3[0]['date']);
@@ -149,16 +149,16 @@ class OneySimulationResourceTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('2020-04-06T00:00:00.000Z', $installments_x3[1]['date']);
         $this->assertEquals(16833, $installments_x3[1]['amount']);
 
-        $this->assertEquals(17565, $simulation->x3_with_fees['down_payment_amount']);
+        $this->assertEquals(17565, $simulations->x3_with_fees['down_payment_amount']);
 
         // check 4x with fees
-        $this->assertObjectHasAttribute('x4_with_fees', $simulation);
-        $this->assertEquals(true, is_array($simulation->x4_with_fees));
-        $this->assertEquals(19.25, $simulation->x4_with_fees['effective_annual_percentage_rate']);
-        $this->assertEquals(17.74, $simulation->x4_with_fees['nominal_annual_percentage_rate']);
-        $this->assertEquals(732, $simulation->x4_with_fees['total_cost']);
+        $this->assertObjectHasAttribute('x4_with_fees', $simulations);
+        $this->assertEquals(true, is_array($simulations->x4_with_fees));
+        $this->assertEquals(19.25, $simulations->x4_with_fees['effective_annual_percentage_rate']);
+        $this->assertEquals(17.74, $simulations->x4_with_fees['nominal_annual_percentage_rate']);
+        $this->assertEquals(732, $simulations->x4_with_fees['total_cost']);
 
-        $installments_x4 = $simulation->x4_with_fees['installments'];
+        $installments_x4 = $simulations->x4_with_fees['installments'];
 
         $this->assertEquals(3, count($installments_x4));
 
@@ -169,6 +169,6 @@ class OneySimulationResourceTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('2020-05-06T00:00:00.000Z', $installments_x4[2]['date']);
         $this->assertEquals(16833, $installments_x4[2]['amount']);
 
-        $this->assertEquals(17565, $simulation->x4_with_fees['down_payment_amount']);
+        $this->assertEquals(17565, $simulations->x4_with_fees['down_payment_amount']);
     }
 }
