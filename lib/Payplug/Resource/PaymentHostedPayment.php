2f57dbc9aa25f5d2c6b3f23d3acb96e80c35531f
Julien Hoarau <jhoarau@payplug.com>
2025-04-18 16:14:26 +0200

    PRE-2828 feat: Generate a Hosted Fields token

--- lib/Payplug/Resource/PaymentHostedPayment.php
+++ lib/Payplug/Resource/PaymentHostedPayment.php
@@ -19,4 +19,36 @@ class PaymentHostedPayment extends APIResource
         $object->initialize($attributes);
         return $object;
     }
+
+    /**
+     * Create a Hosted Fields token.
+     *
+     * @return array
+     */
+	public static function createHFToken()
+	{
+		$httpClient = new Payplug\Core\HttpClient();
+        try {
+            $response = $httpClient->post(
+              'https://staging-hosted-fields-front-service.notprod-cde-gwdl.gcp.dlns.io/service/tokenize',
+              array(
+                  'APIKEYID' => 'fadc44f6-b98b-4ea1-a8a0-50ab1d2e216f',
+                  'SELECTEDBRAND' => 'mastercard',
+                  'CARDVALIDITYDATE' => '12-28',
+                  'CARDCVV' => '123',
+                  'CARDCODE' => '5131080132762421'
+              ),
+              false,
+              'uid=Ck8hGmfi3cbB7AFVAx5bAg==',
+              array(
+                  'Content-Type: text/plain',
+                  'x-test-auto: true'
+              )
+            );
+
+            return $response;
+        } catch (\Exception $e) {
+            return array();
+        }
+	}
 }
